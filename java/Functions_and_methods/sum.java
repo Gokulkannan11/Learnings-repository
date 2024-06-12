199b8d3a6c2ecb8587db7530f058bef39bf1011c
Gokulkannan11 <gokulkannanrm11@gmail.com>
2024-06-12 22:44:37 +0530

    update

--- java/Functions_and_methods/sum.java
+++ java/Functions_and_methods/sum.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
-        sum();
+       int ans =  sum2(); //Function call
+       System.out.println(ans);
         
+    }
+
+    static int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number 1 : ");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2 : ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return sum; // once return is executed the function is over
     }
 
     static void sum () {
