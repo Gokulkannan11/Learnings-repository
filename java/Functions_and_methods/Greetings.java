756ee09b620e835ca625eb332d6999d9d66b99dc
Gokulkannan11 <gokulkannanrm11@gmail.com>
2024-06-17 11:58:50 +0530

    grreting

--- java/Functions_and_methods/Greetings.java
+++ java/Functions_and_methods/Greetings.java
@@ -2,10 +2,13 @@ package Functions_and_methods;
 
 public class Greetings {
     public static void main(String[] args) {
-        greeting() ;
+        String message=  greeting() ;
+        System.out.println(message);
     }
     
-} 
-static void  greeting(){
-    System.out.println("Hello world");
+ 
+static String  greeting(){
+    String greeting = "hey ! how are you ?";
+    return greeting;
+    }
 }
