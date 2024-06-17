1065c22ddcf4d5c550ef531d8abd360a475f5bfc
Gokulkannan11 <gokulkannanrm11@gmail.com>
2024-06-17 22:28:50 +0530

    update

--- java/Functions_and_methods/swap.java
+++ java/Functions_and_methods/swap.java
@@ -2,6 +2,23 @@ package Functions_and_methods;
 
 public class swap {
     public static void main(String[] args) {
+        int a = 20;
+        int b = 10;   
+
+        //swap two numbers
+       // int temp =a;
+       // a= b;
+       // b= temp;
+       swap (a,b);
+
+       System.out.println(a + " " + b);
+
         
-    }
+     }
+
+     static void swap(int a , int b){
+        int temp = a;
+        a= b;
+        b= temp;
+     }
 }
