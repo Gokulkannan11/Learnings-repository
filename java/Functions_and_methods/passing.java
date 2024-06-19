c7b91a1552d00fa0d1cd142e2cea68f9e07da7dc
Gokulkannan11 <gokulkannanrm11@gmail.com>
2024-06-19 23:50:42 +0530

    passing

--- java/Functions_and_methods/passing.java
+++ java/Functions_and_methods/passing.java
@@ -1,5 +1,13 @@
 package Functions_and_methods;
 
 public class passing {
+
+        public static void main(String[] args) {
+            String avenger = "Iron Man";
+            greet(avenger);
+        }
     
-}
+        static void greet(String name) {
+            System.out.println(name);
+        }
+    }
